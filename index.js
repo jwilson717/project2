d59c36f3500b281822cd924fd60f2668551893a6
= <wil16078@byui.edu>
2020-06-24 18:45:49 -0700

    packages

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 app.get('/', function (req, res) {
    res.writeHead(200, {'Content-Type':'text/html'});
    res.write('welcome.html');
+   res.end();
 });
 
 app.listen(PORT);
