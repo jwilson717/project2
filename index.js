d1bc0fcc454c123157f8ac5a3a2191520d8ab205
= <wil16078@byui.edu>
2020-06-25 20:15:47 -0700

    new account page

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8080;
+const bcrypt = require('bcrypt');
 const { Pool } = require('pg');
 const pool = new Pool({connectionString: process.env.DATABASE_URL});
 
@@ -16,15 +17,21 @@ app.get('/newaccount', function (req, res) {
 });
 
 app.post('/newaccount', function (req, res) {
-   pool.connect(function (err, client, done){
-      if(err) throw err;
-      client.query('INSERT INTO accounts (fname, lname, email, username, password) VALUES ($1, $2, $3, $4, $5)', [req.body.fname, req.body.lname, req.body.email, req.body.username, req.body.password], function (err, response) {
-         done();
-         if(err) {
-            res.send(JSON.stringify({status: 'Error', message: 'Error Creating Account'}));
-         } else {
-            res.send(JSON.stringify({status: 'Success', message: 'dashboard'}));
-         }
+   bcrypt.hash(req.body.password, 10, (err, hash)=> {
+      if (err) {
+         console.log(err);
+         return;
+      }
+      pool.connect(function (err, client, done){
+         if(err) throw err;
+         client.query('INSERT INTO accounts (fname, lname, email, username, password) VALUES ($1, $2, $3, $4, $5)', [req.body.fname, req.body.lname, req.body.email, req.body.username, req.body.password], function (err, response) {
+            done();
+            if(err) {
+               res.send(JSON.stringify({status: 'Error', message: 'Error Creating Account'}));
+            } else {
+               res.send(JSON.stringify({status: 'Success', message: 'dashboard'}));
+            }
+         });
       });
    });
 });
