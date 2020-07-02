8f076c22903e8141b20bb375c85483f5ab5ca0a0
= <wil16078@byui.edu>
2020-07-01 18:18:34 -0700

    search

--- db/test.sql
+++ db/test.sql
@@ -5,6 +5,6 @@ FROM movies m INNER JOIN rating r
    ON m.rating_id = r.rating_id LEFT JOIN movie_has_genre mg 
    ON m.movie_id = mg.movie_id LEFT JOIN genres g 
    ON g.genre_id = mg.genre_id INNER JOIN accounts a 
-   ON m.account_id = m.account_id
+   ON m.account_id = a.account_id
 WHERE a.username = 'Test'
 AND (UPPER(m.title) LIKE UPPER('%iron%') OR UPPER(g.genre) LIKE UPPER('%Iron%') OR UPPER(m.description) LIKE UPPER('%iron%'));
