4423100506ef6e516854086d89d2c80ef0206407
= <wil16078@byui.edu>
2020-07-10 21:03:29 -0700

    genre updates

--- index.js
+++ index.js
@@ -188,8 +188,12 @@ app.post('/update', function (req, res) {
             res.json({success: false, msg: 'Error updating movie'});
          } else {
             if (req.body.genres){
+               client.query("DELETE FROM movie_has_genre WHERE movie_id = $1", [req.body.movie_id], function (){
+                  if(err) {console.log(err.stack);}
+               });
                req.body.genres.forEach((genre) => {
-                  client.query("DELETE FROM movie_has_genre WHERE movie_id = $1; INSERT INTO movie_has_genre (movie_id, genre_id) VALUES ($1, (SELECT genre_id FROM genres WHERE genre = $2))", [req.body.movie_id,genre], function (err, result) {
+                  client.query("INSERT INTO movie_has_genre (movie_id, genre_id) VALUES ($1, (SELECT genre_id FROM genres WHERE genre = $2))", [req.body.movie_id,genre], function (err, result) {
+                     done();
                      if (err) {
                         res.json({success: false, msg: 'Genre not correctly added'});
                      } else {
